b07eaf8d03ed3c8a5bf8ac6938f85221b90243fa
ScarFace <nitingupta4592@gmail.com>
2026-07-01 02:02:43 +0530

    added this inset-e-0 in place of end

--- frontend/src/components/messages/MessageInput.jsx
+++ frontend/src/components/messages/MessageInput.jsx
@@ -29,7 +29,7 @@ const MessageInput = () => {
 
         <button
           type="submit"
-          className="absolute inset-y-0 end-0 flex items-center pe-3"
+          className="absolute inset-y-0 inset-e-0 flex items-center pe-3"
           disabled={loading}
         >
           {loading ? (
