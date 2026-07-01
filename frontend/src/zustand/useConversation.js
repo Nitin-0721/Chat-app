99add7ae966f2ac409dbc70ee7aa6f64eda1f78b
ScarFace <nitingupta4592@gmail.com>
2026-07-02 02:49:17 +0530

    corrected backend

--- frontend/src/zustand/useConversation.js
+++ frontend/src/zustand/useConversation.js
@@ -1,15 +1,10 @@
 import { create } from "zustand";
 
 const useConversation = create((set) => ({
-  selectedConversation: null,
-  setSelectedConversation: (selectedConversation) =>
-    set({ selectedConversation }),
-
-  messages: [],
-  setMessages: (messages) => set({ messages }),
-
-  conversations: [],
-  setConversations: (conversations) => set({ conversations }),
+	selectedConversation: null,
+	setSelectedConversation: (selectedConversation) => set({ selectedConversation }),
+	messages: [],
+	setMessages: (messages) => set({ messages }),
 }));
 
 export default useConversation;
